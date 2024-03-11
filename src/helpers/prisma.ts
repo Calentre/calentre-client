e1b759c80c50dbd9e49ff513c6ae8e5744f44bb7
Jose Luis Flores Fuentes <floresfuentes93.alter@gmail.com>
2024-03-11 12:05:01 -0600

    removing unnecessary declaration

--- src/helpers/prisma.ts
+++ src/helpers/prisma.ts
@@ -5,12 +5,6 @@ const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+const prisma = prismaClientSingleton();
 
 export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
